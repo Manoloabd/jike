72f61c7a46dbfa77737d4509b17dbfe705449d61
user1 <user1@lab3.com>
2022-06-27 15:31:12 +0800

    展示默认的内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import './App.scss';
-
-function App () {
-  return <div className="app">
-  </div>;
+import './App.scss'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+import Layout from './pages/Layout'
+import Login from './pages/Login'
+function App() {
+  return (
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route
+            path='/'
+            exact
+            render={() => <Redirect to='/home'></Redirect>}
+          ></Route>
+          <Route path='/home' component={Layout}></Route>
+          <Route path='/login' component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
