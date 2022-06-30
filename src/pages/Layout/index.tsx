6cf0cd8a859b208be2ecde864b638bfde91d3892
user1 <user1@lab3.com>
2022-06-30 14:04:58 +0800

    字体图标

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default function index() {
   return (
     <div>
-      布局
-      <br />
+      <svg className='icon' aria-hidden='true'>
+        xlinkHref指的是 字体图标的类型
+        <use xlinkHref='#iconbtn_like_sel' />
+      </svg>
     </div>
   )
 }
