8adcf5c0e899995656d17ce310427f59dc4243a4
user1 <user1@lab3.com>
2022-06-30 15:20:21 +0800

    创建store

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom'
 import '@/index.scss'
 import App from '@/App'
-ReactDOM.render(<App />, document.getElementById('root'))
+import { Provider } from 'react-redux'
+import store from '@/store'
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
