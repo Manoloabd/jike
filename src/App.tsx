5e8edb9aa4e11ce3bd1c7f0436d221430140552f
user1 <user1@lab3.com>
2022-07-01 15:19:39 +0800

    ult-history

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import '@/App.scss'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
+import { customHistory } from './utils/history'
+
 function App() {
   return (
-    <Router>
+    <Router history={customHistory}>
       <div className='app'>
         <Switch>
           <Route
